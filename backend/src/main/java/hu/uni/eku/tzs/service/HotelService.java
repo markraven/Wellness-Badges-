5877669c1cbda90c550f087966be776b16530945
FeketeSzabolcs <fekszabolcs@gmail.com>
2020-11-15 10:44:50 +0100

    Refactored Swagger annotations into Controller

--- backend/src/main/java/hu/uni/eku/tzs/service/HotelService.java
+++ backend/src/main/java/hu/uni/eku/tzs/service/HotelService.java
@@ -4,25 +4,19 @@ import hu.uni.eku.tzs.dao.entity.ProductsServices;
 import hu.uni.eku.tzs.dao.entity.Purchase;
 import hu.uni.eku.tzs.dao.entity.Reservation;
 import hu.uni.eku.tzs.model.*;
-import io.swagger.annotations.ApiOperation;
 
 import java.time.LocalDate;
 import java.util.Set;
 
 public interface HotelService {
 
-    @ApiOperation("Creates a reservation if there's a free room for the guests")
     Reservation bookRoom(LocalDate startDate, LocalDate endDate, int guests);
 
-    @ApiOperation("Check-in to hotel")
     Set<GuestDto> checkIn(ReservationCheckInDto reservation);
 
-    @ApiOperation("A Guest purchases an item or a service")
     Purchase buyProductOrService(BuyDto buyDto);
 
-    @ApiOperation("Get invoice for room")
     Invoice getInvoiceForRoom(Long reservationId);
 
-    @ApiOperation("Add new Product or Service")
     ProductsServices addNewBillable(ProductServiceDto productServiceDto);
 }
